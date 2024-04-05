8b674906c38d5b782a2afad415e2468c904bb207
Mei Huang <meihuang8328@gmail.com>
2024-04-05 11:48:12 -0700

    winner function completed

--- index.js
+++ index.js
@@ -17,4 +17,21 @@ function randomChoice() {
     }
 }
 
+function winner(player1, player2) {
+
+    if (player1 === player2) {
+        console.log("It's a tie");
+        return "It's a tie";
+    } else if ((player1 === "rock" && player2 === "scissors") || (player1 === "scissors" && player2 === 'paper') || (player1 === "paper" && player2 === "rock")) {
+        console.log('Player 1 Wins!');
+        return "Player 1 Wins!";
+    } else {
+        console.log("Player 2 Wins!");
+        return "Player 2 Wins!";
+    }
+}
+
+
+
+
 randomChoice();
