559513adcb92889a1faa9b7585c9271ef7d3a18b
Mei Huang <meihuang8328@gmail.com>
2024-04-05 11:31:09 -0700

    finished randomChoice

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ console.log('hello world');
 
 function randomChoice() {
     let choice = Math.floor(Math.random() * 3);
-    console.log(choice);
+    // console.log(choice);
+
+    if (choice === 0) {
+        // console.log("rock")
+        return "rock"
+    } else if (choice === 1) {
+        // console.log('paper')
+        return "paper"
+    } else {
+        // console.log('scissors')
+        return "scissors"
+    }
 }
 
 randomChoice();
